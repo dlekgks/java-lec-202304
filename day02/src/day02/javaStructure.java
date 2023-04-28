990426405c0ae8dacdb79849816f38e3fb1d334e
사과주스 <darkfool66@gmail.com>
2023-04-28 22:08:26 +0900

    2일차 기타등등

---
+++ day02/src/day02/javaStructure.java
@@ -0,0 +1,15 @@
+package day02;
+
+//컴파일 에러를 발생시키는 기초 문법
+//1. 자바는 클래스 이름이 같아야 합니다.
+//2. 자마는 대소문자를 구분합니다.
+// ex)Java java -> 같지 않아요
+//3. {} 중괄호로 문단,문장을 구분함니다.
+//4. 자바의 문장의 끝은 ;을 사용합니다.
+
+public class javaStructure 
+{
+ public static void main(String args[]) {
+	 System.out.println("하악하악");
+ }
+}
